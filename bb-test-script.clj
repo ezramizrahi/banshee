588b62e96d62f76c482423121b207567120ae5cb
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-14 19:34:04 +1100

    test

--- bb-test-script.clj
+++ bb-test-script.clj
@@ -9,5 +9,8 @@
 (when (fs/exists? "data.json")
   (println "File exists")
   (println (hiccup/html [:span {:class "foo"} "bar"]))
+  (def movies (map :movie (json/parse-string (slurp "data.json"))))
+  (println movies)
+  (spit "textfile.txt" (interpose "\n" movies))
   (println (json/parse-string (slurp "data.json") true)))
 (fs/cwd)
