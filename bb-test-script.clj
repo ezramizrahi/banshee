8c8b92d9bd81630da6b511576c14a38c65a20e85
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-14 18:54:04 +1100

    test

--- bb-test-script.clj
+++ bb-test-script.clj
@@ -3,9 +3,11 @@
 (ns bb-test-script
   (:require
    [babashka.fs :as fs]
+   [babashka.hiccup :as html]
    [cheshire.core :as json]))
 
 (when (fs/exists? "data.json")
   (println "File exists")
+  (html [:span {:class "foo"} "bar"])
   (println (json/parse-string (slurp "data.json") true)))
 (fs/cwd)
