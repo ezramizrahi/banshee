c73d535668a679105232844c46fc3832f92a7ff7
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-15 11:53:46 +1100

    update workflow:

--- bb-test-script.clj
+++ bb-test-script.clj
@@ -3,14 +3,16 @@
 (ns bb-test-script
   (:require
    [babashka.fs :as fs]
+   [babashka.curl :as curl]
    [hiccup.core :as hiccup]
    [cheshire.core :as json]))
 
-(when (fs/exists? "data.json")
-  ;; (println "File exists")
+;; (when (fs/exists? "data.json")
+;;   (println "File exists"))
   ;; (println (hiccup/html [:span {:class "foo"} "bar"]))
   ;; (def movies (map #(:movie %) (json/parse-string (slurp "data.json"))))
   ;; (println movies)
   ;; (spit "textfile.txt" (interpose "\n" movies))
-  (println (json/parse-string (slurp "data.json") true)))
-(fs/cwd)
+  ;; (println (json/parse-string (slurp "data.json") true)))
+(def resp (curl/get "https://banshee.netlify.app/.netlify/functions/get_movies"))
+(println (json/parse-string (:body resp))) ;;=> {"code" 200, "description" "OK"}
