1ac9b0b70f95618086dbc7e85af421cb9d4c070f
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-24 06:30:02 +1100

    update names

--- scrape.test.js
+++ scrape.test.js
@@ -1,30 +1,31 @@
 const data = require('./data.json');
 
-test('movie key value is a string', () => {
+test('movie is a string', () => {
     data.forEach(d => {
         expect(d.movie).toEqual(expect.any(String));
     });
 });
 
-test('rating key value is a string', () => {
+test('rating is a string', () => {
     data.forEach(d => {
         expect(d.rating).toEqual(expect.any(String));
     });
 });
 
-test('summary key value is a string', () => {
+test('summary is a string', () => {
     data.forEach(d => {
         expect(d.summary).toEqual(expect.any(String));
     });
 });
 
-test('times key value is an array of strings', () => {
+test('times an array of strings', () => {
     data.forEach(d => {
         expect(d.times).toEqual(expect.any(Array));
+        expect(d.times).toEqual(expect.arrayContaining(expect.any(String)));
     });
 });
 
-test('that scraped_at key exists in all objs', () => {
+test('scraped_at is a string', () => {
     data.forEach(d => {
         expect(d.scraped_at).toEqual(expect.any(String));
     });
