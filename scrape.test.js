a5055ca9084927f457b4f9f243bb09705abccf99
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-24 06:38:43 +1100

    update tests

--- scrape.test.js
+++ scrape.test.js
@@ -2,31 +2,36 @@ const data = require('./data.json');
 
 test('movie is a string', () => {
     data.forEach(d => {
+        expect(d).toHaveProperty('movie');
         expect(d.movie).toEqual(expect.any(String));
     });
 });
 
 test('rating is a string', () => {
     data.forEach(d => {
+        expect(d).toHaveProperty('rating');
         expect(d.rating).toEqual(expect.any(String));
     });
 });
 
 test('summary is a string', () => {
     data.forEach(d => {
+        expect(d).toHaveProperty('summary');
         expect(d.summary).toEqual(expect.any(String));
     });
 });
 
 test('times an array of strings', () => {
     data.forEach(d => {
+        expect(d).toHaveProperty('times');
         expect(d.times).toEqual(expect.any(Array));
-        expect(d.times).toEqual(expect.arrayContaining(expect.any(String)));
+        expect(d.times).toEqual(expect.arrayContaining([expect.any(String)]));
     });
 });
 
 test('scraped_at is a string', () => {
     data.forEach(d => {
+        expect(d).toHaveProperty('scraped_at');
         expect(d.scraped_at).toEqual(expect.any(String));
     });
 });
