648d7b521229ec0bdcf1dc2b934b216d307fcea3
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-26 16:12:14 +1100

    add axios; add env; use rest api instead of scraping

--- scrape.js
+++ scrape.js
@@ -1,12 +1,23 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const dayjs = require('dayjs');
+const axios = require('axios');
 
 (async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setViewport({ width: 0, height: 0});
     await page.setJavaScriptEnabled(false);
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+        if(req.resourceType() === 'image'){
+            req.abort();
+        }
+        else {
+            req.continue();
+        }
+    });
     await page.goto('https://www.ritzcinemas.com.au/now-showing', { waitUntil: 'load' });
 
     // Store movie titles from the Ritz now-showing page
@@ -38,49 +49,22 @@ const dayjs = require('dayjs');
         }
     }
 
+    // close browser
+    await browser.close();
+
     let ratings = [];
     let summaries = [];
     for (let index = 0; index < movieTitles.length; index++) {
-        // Search themoviedb for each film
-        await page.goto('https://www.themoviedb.org/', { waitUntil: 'load' });
-        await page.screenshot({path: 'what.png'});
-        await page.waitForSelector('#inner_search_v4', { timeout: 35000, visible: true });
-        await page.type('#inner_search_v4', movieTitles[index], { delay: 100 });
-        await Promise.all([
-            page.waitForNavigation({ waitUntil: "load" }),
-            page.keyboard.press("Enter")
-        ]);
-
-        // Click on the first search result
-        // brittle, but works for now
-        // else add a default rating and summary
-        const searchResults = await page.$$('.image');
-        if (searchResults.length > 0) {
-            await Promise.all([
-                page.waitForNavigation({ waitUntil: "load" }),
-                await searchResults[0].click()
-            ]);
-        } else {
-            console.log(`${movieTitles[index]} not found`);
-            ratings.push(`Rating for ${movieTitles[index]} not found`);
-            summaries.push(`Summary for ${movieTitles[index]} not found`);
-            // Finish here and continue with the next iteration
-            continue;
-        }
-
-        // Get film rating
-        let rating = await page.$$eval(".user_score_chart", el => el.map(x => x.getAttribute("data-percent")));
-        ratings.push(rating[0].slice(0, -2) + "/100");
-        // Get film summary
-        let summaryParentElem = await page.$('.overview');
-        let summaryChildElem = await summaryParentElem.$$(':scope > *');
-        let summary = await page.evaluate(el => el.textContent, summaryChildElem[0]);
-        summaries.push(summary);
+        const uriEncodedComponentTitle = encodeURIComponent(movieTitles[index].trim());
+        const queryURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${uriEncodedComponentTitle}&page=1&include_adult=false`;
+        const res = await axios.get(queryURL);
+        const movieObj = res.data.results.find((r) => r.title.normalize('NFD').replace(/\p{Diacritic}/gu, "").trim() === movieTitles[index].trim());
+        const tempRating = movieObj ? movieObj.popularity.toString() : 'unknown';
+        const tempSummary = movieObj ? movieObj.overview : 'unknown';
+        ratings.push(tempRating);
+        summaries.push(tempSummary);
     }
-    // Close the browser
-    await browser.close();
 
-    // const scrapedAt = new Date().toISOString().split("T")[0];
     const scrapedAt = dayjs().format('dddd, MMMM D, YYYY h:mm A');
     // Create an array of objects containing film title and rating
     let output = movieTitles.map((movie,i) => ({ movie, rating: ratings[i], summary: summaries[i], times: nowShowingSessions[i], scraped_at: scrapedAt }));
