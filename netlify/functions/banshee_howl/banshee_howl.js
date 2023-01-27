d6b169ffb0b2aeb99c796a859238826a2823f2b8
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-27 13:20:00 +1100

    update fn

--- netlify/functions/banshee_howl/banshee_howl.js
+++ netlify/functions/banshee_howl/banshee_howl.js
@@ -1,38 +1,13 @@
 
 const { Telegraf } = require("telegraf");
-const { MongoClient } = require("mongodb");
 const axios = require('axios');
-
-const mongoClient = new MongoClient(process.env.ATLAS_URI);
-const clientPromise = mongoClient.connect();
 const json2html = require('node-json2html');
 
-// create new bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // let html = json2html.transform(data,{"<>": "li", "html":[
 //     {"<>": "span", "text": "${movie} ${summary}"}
 // ]});
-// console.log(html)
-// let mymsg;
-// async function getMyData() {
-//     try {
-//         const res = await axios.get(
-//             `https://banshee.netlify.app/.netlify/functions/get_movies`
-//         );
-//         console.log('data', res);
-//     } catch (err) {
-//         console.log(err);
-//     }
-
-//     // testing if i can get data
-//     if (res) {
-//         return mymsg = 'ive got data';
-//     } else {
-//         return mymsg = 'no data';
-//     }
-// };
-// getMyData();
 
 bot.start(ctx => {
   console.log("Received /start command")
@@ -43,23 +18,23 @@ bot.start(ctx => {
     return ctx.reply("Error occured")
   }
 });
+
 bot.hears('howl', async function (ctx, next) {
     const res = await axios.get(
         `https://banshee.netlify.app/.netlify/functions/get_movies`
     );
-    console.log('data', res.data);
-    let mymsg;
-    if (res.data) {
-        mymsg = 'i have data'
+    let movieTimes
+    if (res.data.length > 1) {
+        movieTimes = res.data.filter((d) => d.bot_text).join('\n');
     } else {
-        mymsg = 'no data'
+        movieTimes = 'No data yet';
     }
-
     await ctx.telegram.sendMessage(ctx.message.chat.id,
-        `hi ${ctx.message.chat.first_name}! ${mymsg}`,
+        `hi ${ctx.message.chat.first_name}! ${movieTimes}`,
         { parse_mode: 'HTML' }
     )
 });
+
 bot.on('message', async function (ctx, next) {
     await ctx.telegram.sendMessage(ctx.message.chat.id,
         "<b>Babylon A tale of outsized ambition and outrageous excess tracing the rise and fall of multiple characters in an era of unbridled decadence and depravity during Hollywood</b>",
@@ -71,9 +46,6 @@ bot.on('message', async function (ctx, next) {
 const handler = async event => {
   try {
     await bot.handleUpdate(JSON.parse(event.body));
-    // const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
-    // const collection = database.collection(process.env.MONGODB_COLLECTION);
-    // const results = await collection.find({}).limit(30).toArray();
     return { statusCode: 200, body: "" }
   } catch (e) {
     console.error("error in handler:", e)
