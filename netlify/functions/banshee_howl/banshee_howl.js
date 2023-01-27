a37f1fb8fddc9d7c40f1f84842ca0e23d8bcacf4
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-27 12:55:15 +1100

    async await

--- netlify/functions/banshee_howl/banshee_howl.js
+++ netlify/functions/banshee_howl/banshee_howl.js
@@ -14,25 +14,25 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 //     {"<>": "span", "text": "${movie} ${summary}"}
 // ]});
 // console.log(html)
-let mymsg;
-async function getMyData() {
-    try {
-        const res = await axios.get(
-            `https://banshee.netlify.app/.netlify/functions/get_movies`
-        );
-        console.log('data', res);
-    } catch (err) {
-        console.log(err);
-    }
+// let mymsg;
+// async function getMyData() {
+//     try {
+//         const res = await axios.get(
+//             `https://banshee.netlify.app/.netlify/functions/get_movies`
+//         );
+//         console.log('data', res);
+//     } catch (err) {
+//         console.log(err);
+//     }
 
-    // testing if i can get data
-    if (res) {
-        return mymsg = 'ive got data';
-    } else {
-        return mymsg = 'no data';
-    }
-};
-getMyData();
+//     // testing if i can get data
+//     if (res) {
+//         return mymsg = 'ive got data';
+//     } else {
+//         return mymsg = 'no data';
+//     }
+// };
+// getMyData();
 
 bot.start(ctx => {
   console.log("Received /start command")
@@ -43,14 +43,29 @@ bot.start(ctx => {
     return ctx.reply("Error occured")
   }
 });
-bot.hears('howl', function (ctx, next) {
-    ctx.telegram.sendMessage(ctx.message.chat.id,
+bot.hears('howl', async function (ctx, next) {
+    try {
+        const res = await axios.get(
+            `https://banshee.netlify.app/.netlify/functions/get_movies`
+        );
+        console.log('data', res);
+    } catch (err) {
+        console.log(err);
+    }
+    let mymsg;
+    if (res) {
+        mymsg = 'i have data'
+    } else {
+        mymsg = 'no data'
+    }
+
+    await ctx.telegram.sendMessage(ctx.message.chat.id,
         `hi ${ctx.message.chat.first_name}! ${mymsg}`,
         { parse_mode: 'HTML' }
     )
 });
-bot.on('message', function (ctx, next) {
-    ctx.telegram.sendMessage(ctx.message.chat.id,
+bot.on('message', async function (ctx, next) {
+    await ctx.telegram.sendMessage(ctx.message.chat.id,
         "<b>Babylon A tale of outsized ambition and outrageous excess tracing the rise and fall of multiple characters in an era of unbridled decadence and depravity during Hollywood</b>",
       { parse_mode: 'HTML' }
     )
